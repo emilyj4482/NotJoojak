835a769fde9b46e861c8b825457f7bcd545aa8cd
Hamin Kim <dev.hamin.kim@gmail.com>
2024-10-22 19:31:09 +0900

    Added Member structures and basic stuffs

--- NotJoojak/MemberDetailViewController.swift
+++ NotJoojak/MemberDetailViewController.swift
@@ -9,21 +9,40 @@ import UIKit
 
 class MemberDetailViewController: UIViewController {
 
+    var member: Member?
+
+    @IBOutlet weak var profilePicture: UILabel!
+    @IBOutlet weak var backgroundPicture: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var age: UILabel!
+    @IBOutlet weak var MBTI: UILabel!
+    @IBOutlet weak var hobby: UILabel!
+    @IBOutlet weak var interests: UILabel!
+    @IBOutlet weak var workingStyle: UILabel!
+    @IBOutlet weak var devExperience: UILabel!
+    @IBOutlet weak var myStrength: UILabel!
+    @IBOutlet weak var blogURL: UILabel!
+    @IBOutlet weak var githubURL: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let member = member {
+            profilePicture.text = member.profilePicture
+            backgroundPicture.text = member.backgroundPicture
+            name.text = member.name
+            MBTI.text = member.MBTI.rawValue
+            age.text = String(member.age)
+            hobby.text = member.hobby.first ?? "Nothing"
+            interests.text = member.interests.first ?? "Nothing"
+            workingStyle.text = member.workingStyle
+            devExperience.text = member.devExperience
+            myStrength.text = member.myStrength
+            blogURL.text = member.blogURL
+            githubURL.text = member.githubURL
+        } else {
+            print("No data received")
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
