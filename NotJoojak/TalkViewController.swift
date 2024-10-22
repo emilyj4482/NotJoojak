d4880e90cf6511074d6d86d349fc585a900757bb
jeff <vnvn0324@naver.com>
2024-10-22 21:48:06 +0900

    talk storyboard 1차 제작

--- NotJoojak/TalkViewController.swift
+++ NotJoojak/TalkViewController.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 class TalkViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
