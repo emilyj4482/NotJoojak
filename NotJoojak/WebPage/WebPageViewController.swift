3f55e2f52ec479255f1c00c9a2621deae50fc1d5
emilyj4482 <emilyj4482@gmail.com>
2024-10-22 16:47:51 +0900

    web view 화면 완성

--- NotJoojak/WebPage/WebPageViewController.swift
+++ NotJoojak/WebPage/WebPageViewController.swift
@@ -6,13 +6,22 @@
 //
 
 import UIKit
+import WebKit
 
 class WebPageViewController: UIViewController {
     
-    @IBOutlet var backgroundView: UIView!
+    let url: String = "https://github.com/emilyj4482"
+
+    @IBOutlet var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadWebView(to: url)
+    }
+    
+    private func loadWebView(to url: String) {
+        guard let url = URL(string: url) else { return }
+        webView.load(URLRequest(url: url))
     }
     
     @IBAction func closeButtonTapped(_ sender: UIButton) {
