749682725b5d04bf54157eadf00add0fe001b55f
gyeom <gyeomii95@naver.com>
2024-10-22 15:59:34 +0900

    팀 디테일 구성

--- NotJoojak/MemberDetailViewController.swift
+++ NotJoojak/MemberDetailViewController.swift
@@ -8,22 +8,32 @@
 import UIKit
 
 class MemberDetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var Background: UIImageView!
+    @IBOutlet weak var miniprofile: UIImageView!
+    @IBOutlet weak var firstBox: UIView!
+    @IBOutlet weak var secondBox: UIView!
+    @IBOutlet weak var lastBox: UIView!
+    @IBOutlet weak var blogBox: UIButton!
+    @IBOutlet weak var gitBox: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        //미니프로필 사진
+        miniprofile.layer.cornerRadius = miniprofile.frame.height / 2
+        //프로필 박스
+        firstBox.layer.cornerRadius = firstBox.frame.height / 2
+        secondBox.layer.cornerRadius = 30
+        lastBox.layer.cornerRadius = lastBox.frame.height / 2
+        //블로그 박스
+        blogBox.layer.cornerRadius = blogBox.frame.height / 2
+        gitBox.layer.cornerRadius = gitBox.frame.height / 2
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
