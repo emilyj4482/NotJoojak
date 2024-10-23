0b59e7c26826e7e54d3c31a7ba10220935697fd0
emilyj4482 <emilyj4482@gmail.com>
2024-10-23 13:44:43 +0900

    modal present 적용한 뒤 auto layout 다시 적용, label box 색상에 투명도 추가

--- NotJoojak/TeamDetailViewController.swift
+++ NotJoojak/TeamDetailViewController.swift
@@ -9,8 +9,18 @@ import UIKit
 
 class TeamDetailViewController: UIViewController {
     
+    @IBOutlet var firstLabelBox: UIView!
+    @IBOutlet var secondLabelBox: UIView!
+    @IBOutlet var thirdLabelBox: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setBoxColor()
     }
     
+    private func setBoxColor() {
+        firstLabelBox.backgroundColor = UIColor.darkGray.withAlphaComponent(0.8)
+        secondLabelBox.backgroundColor = UIColor.darkGray.withAlphaComponent(0.8)
+        thirdLabelBox.backgroundColor = UIColor.darkGray.withAlphaComponent(0.8)
+    }
 }
