3ae904d5ebb940cd7a80eb2855561f5282be9bfa
emilyj4482 <emilyj4482@gmail.com>
2024-10-23 11:28:59 +0900

    민지 section까지 auto layout 적용 완료

--- NotJoojak/TeamDetailViewController.swift
+++ NotJoojak/TeamDetailViewController.swift
@@ -9,16 +9,8 @@ import UIKit
 
 class TeamDetailViewController: UIViewController {
     
-    @IBOutlet var backgroundImageView: UIImageView!
-    @IBOutlet var minjiImageView: UIImageView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupImageAsset()
     }
-
-    private func setupImageAsset() {
-        backgroundImageView.image = UIImage(named: "teamDetailBackground")
-        minjiImageView.image = UIImage(named: "teamDetailMinji")
-    }
+    
 }
